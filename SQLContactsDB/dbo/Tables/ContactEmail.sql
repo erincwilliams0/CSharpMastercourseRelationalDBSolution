246f9c3f92e16a760531d59d288112c4d4f3fbcf
erin williams <erin.cwilliams0@gmail.com>
2025-09-27 11:14:52 -0500

    feat: Added contact email/phone number tables

---
+++ SQLContactsDB/dbo/Tables/ContactEmail.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ContactEmail]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ContactId] INT NOT NULL, 
+    [EmailId] INT NOT NULL,
+    FOREIGN KEY (ContactId) REFERENCES Contacts(Id),
+    FOREIGN KEY (EmailId) REFERENCES EmailAddresses(Id),
+
+)
---
+++ SQLContactsDB/dbo/Tables/ContactPhoneNumber.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ContactPhoneNumber]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ContactId] INT NOT NULL, 
+    [PhoneNumberId] INT NOT NULL,
+    FOREIGN KEY (ContactId) REFERENCES Contacts(Id),
+    FOREIGN KEY (PhoneNumberId) REFERENCES PhoneNumbers(Id)
+)
