a00603ad80feb74580171ed40a6f7a4fb86cd482
erin williams <erin.cwilliams0@gmail.com>
2025-09-27 12:14:52 -0500

    feat: installed configuration.json package and manually added top level statements to program.cs

--- SQLServerUI/Program.cs
+++ SQLServerUI/Program.cs
@@ -1,3 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.Extensions.Configuration;
 
-Console.ReadLine();
+namespace SQLServerUI;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(GetConnectionString());
+
+        Console.ReadLine();
+    }
+    private static string GetConnectionString(string connectionStringName = "Default")
+    {
+        string? output = "";
+
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+        var config = builder.Build();
+
+        output = config.GetConnectionString(connectionStringName);
+
+        return output;
+    }
+
+}
+
+
