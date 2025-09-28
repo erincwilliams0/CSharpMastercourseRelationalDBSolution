0757f40c091b9e252f081e5c5b9487c398451c91
erin williams <erin.cwilliams0@gmail.com>
2025-09-28 17:16:57 -0500

    feat: created methods ReadAllContacts and ReadContact which calls the DataAccessLibraries GetAllContacts and GetFullContactById

--- SQLServerUI/Program.cs
+++ SQLServerUI/Program.cs
@@ -1,14 +1,36 @@
 ﻿using System;
 using Microsoft.Extensions.Configuration;
+using DataAccessLibrary;
+using DataAccessLibrary.Models;
 
 namespace SQLServerUI;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(GetConnectionString());
+        SqlCrud sql = new SqlCrud(GetConnectionString());
+
+        //ReadAllContacts(sql);
+        //ReadAllContact(sql, 1);
 
         Console.ReadLine();
+    }
+
+    private static void ReadAllContacts(SqlCrud sql)
+    {
+        var rows = sql.GetAllContacts();
+
+        foreach (var row in rows)
+        {
+            Console.WriteLine($"{row.Id}: {row.FirstName} {row.LastName}");
+        }
+    }
+
+    private static void ReadContact(SqlCrud sql, int contactId)
+    {
+        var contact = sql.GetFullContactById(contactId);
+
+        Console.WriteLine($"{contact.BasicInfo.Id}: {contact.BasicInfo.FirstName} {contact.BasicInfo.LastName}");
     }
     private static string GetConnectionString(string connectionStringName = "Default")
     {
