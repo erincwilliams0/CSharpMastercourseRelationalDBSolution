45b66a1ed9052abe4bb0cbb595d44af8d9078687
erin williams <erin.cwilliams0@gmail.com>
2025-09-28 17:15:03 -0500

    feat: created two read methods, getAllContacts and GetFullContactById

---
+++ DataAccessLibrary/SqlCrud.cs
@@ -0,0 +1,54 @@
+﻿using DataAccessLibrary.Models;
+
+namespace DataAccessLibrary
+{
+    public class SqlCrud
+    {
+        private string _connectionString;
+        private SqlDataAccess db = new SqlDataAccess();
+
+        public SqlCrud(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<BasicContactModel> GetAllContacts()
+        {
+            string sql = "select Id, FirstName, LastName from dbo.Contacts";
+
+            return db.LoadData<BasicContactModel, dynamic>(sql, new { }, _connectionString);
+        }
+
+        public FullContactModel GetFullContactById(int id)
+        {
+            string sql = "select Id, FirstName, LastName from dbo.Contacts where Id = @Id";
+            FullContactModel output = new();
+
+            output.BasicInfo = db.LoadData<BasicContactModel, dynamic>(sql, new { Id = id }, _connectionString).FirstOrDefault();
+
+            if (output.BasicInfo == null)
+            {
+                // do something to tell the user that the record was not found
+                throw new Exception("User not found");
+                return null;
+            }
+
+            sql = @"select e.*
+                from dbo.EmailAddresses e
+                join dbo.ContactEmail ce on ce.EmailId = e.Id
+                where ce.ContactId = @Id";
+
+            output.EmailAddresses = db.LoadData<EmailAddressModel, dynamic>(sql, new { Id = id }, _connectionString);
+
+            sql = "select pn.* " +
+                "from dbo.PhoneNumbers pn " +
+                "join dbo.ContactPhoneNumber cpn on cpn.PhoneNumberId = pn.Id " +
+                "where cpn.ContactId = @Id ";
+
+            output.PhoneNumbers = db.LoadData<PhoneNumberModel, dynamic>(sql, new { Id = id }, _connectionString);
+
+            return output;
+
+        }
+    }
+}
